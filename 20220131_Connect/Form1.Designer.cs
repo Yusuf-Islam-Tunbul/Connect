bb0ad692e9de94c24bc4072d99d3101f2bb4abba
Yusuf-Islam-Tunbul <50180280+Yusuf-Islam-Tunbul@users.noreply.github.com>
2022-02-23 21:20:00 +0300

    Proje dosyası ekle.

---
+++ 20220131_Connect/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _20220131_Connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insert_button = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.update_button = new System.Windows.Forms.Button();
+            this.name_textbox = new System.Windows.Forms.TextBox();
+            this.phone_textbox = new System.Windows.Forms.TextBox();
+            this.datagridview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insert_button
+            // 
+            this.insert_button.Location = new System.Drawing.Point(12, 12);
+            this.insert_button.Name = "insert_button";
+            this.insert_button.Size = new System.Drawing.Size(75, 23);
+            this.insert_button.TabIndex = 0;
+            this.insert_button.Text = "KAYDET";
+            this.insert_button.UseVisualStyleBackColor = true;
+            this.insert_button.Click += new System.EventHandler(this.insert_button_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(12, 70);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(75, 23);
+            this.delete_button.TabIndex = 1;
+            this.delete_button.Text = "SİL";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // update_button
+            // 
+            this.update_button.Location = new System.Drawing.Point(12, 41);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(75, 23);
+            this.update_button.TabIndex = 2;
+            this.update_button.Text = "GÜNCELLE";
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // name_textbox
+            // 
+            this.name_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.name_textbox.Location = new System.Drawing.Point(103, 12);
+            this.name_textbox.Multiline = true;
+            this.name_textbox.Name = "name_textbox";
+            this.name_textbox.Size = new System.Drawing.Size(181, 35);
+            this.name_textbox.TabIndex = 3;
+            // 
+            // phone_textbox
+            // 
+            this.phone_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.phone_textbox.Location = new System.Drawing.Point(103, 58);
+            this.phone_textbox.Multiline = true;
+            this.phone_textbox.Name = "phone_textbox";
+            this.phone_textbox.Size = new System.Drawing.Size(181, 35);
+            this.phone_textbox.TabIndex = 4;
+            // 
+            // datagridview
+            // 
+            this.datagridview.AllowUserToAddRows = false;
+            this.datagridview.AllowUserToDeleteRows = false;
+            this.datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridview.Location = new System.Drawing.Point(12, 105);
+            this.datagridview.Name = "datagridview";
+            this.datagridview.ReadOnly = true;
+            this.datagridview.Size = new System.Drawing.Size(343, 133);
+            this.datagridview.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 249);
+            this.Controls.Add(this.datagridview);
+            this.Controls.Add(this.phone_textbox);
+            this.Controls.Add(this.name_textbox);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.insert_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insert_button;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.TextBox name_textbox;
+        private System.Windows.Forms.TextBox phone_textbox;
+        private System.Windows.Forms.DataGridView datagridview;
+    }
+}
+
---
+++ 20220131_Connect/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _20220131_Connect
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Server=DESKTOP-2JD3BUB; Database= Northwind; Trusted_Connection=True");
+
+        string name;
+        string phone;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillTable();
+        }
+
+        void GetData()
+        {
+            name = name_textbox.Text;
+            phone= phone_textbox.Text;
+        }
+
+        bool Exists()
+        {
+            bool exists;
+
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand check_command = new SqlCommand($"select * from shippers where companyname='{name}'", connection);            
+
+            SqlDataReader reader = check_command.ExecuteReader();
+            
+
+            if (reader.HasRows)
+            {
+                exists = true;
+            }
+
+            else
+            {
+                exists = false;
+            }
+
+            connection.Close();
+
+            return (exists);           
+        }
+
+        public void FillTable()
+        {
+            connection.Open();
+
+            DataSet shippers = new DataSet();
+            SqlDataAdapter data_adapter = new SqlDataAdapter("select * from shippers", connection);
+            data_adapter.Fill(shippers);
+            datagridview.DataSource = shippers.Tables[0];
+
+            connection.Close();
+        }
+
+        void Execute(string command_text,SqlConnection connection)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand target_command = new SqlCommand(command_text, connection);
+            target_command.ExecuteReader();
+
+            connection.Close();
+        }
+
+        private void insert_button_Click(object sender, EventArgs e)
+        {
+            GetData();
+            InsertMethod();
+            FillTable();
+        }
+
+        void InsertMethod()
+        {
+            if (name.Trim() == "")
+            {
+                MessageBox.Show("Lütfen şirket ismi giriniz.");
+            }
+
+            else if (Exists())
+            {
+                MessageBox.Show("Zaten bu isimde bir şirket var. İsterseniz telefon numarasını güncelleyebilirsiniz.");
+            }
+
+            else
+            {
+                Execute(($"Insert into shippers values('{name}',{phone})"), connection);
+            }            
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            GetData();
+            Update_();
+            FillTable();
+        }
+
+        void Update_()
+        {
+            if (Exists())
+            {
+                Execute($"Update shippers set phone={phone} where companyname='{name}'", connection);
+            }
+
+            else
+            {
+                MessageBox.Show("Bu isimde bir şirket bulunmamaktadır. İsterseniz yeni kayıt oluşturabilirsiniz.");
+            }            
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            GetData();
+            Delete();
+            FillTable();
+        }
+
+        void Delete()
+        {
+            if (Exists())
+            {
+                Execute($"Delete from shippers where companyname='{name}'", connection);
+            }
+
+            else
+            {
+                MessageBox.Show("Böyle bir şirket zaten bulunmamaktadır.");
+            }            
+        }
+    }
+}
